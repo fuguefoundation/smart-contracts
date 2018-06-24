0c2aee880da402acbcc65719373aa1b0d7f8d579
David Houston <fugueweb@gmail.com>
2018-06-24 16:01:06 +0800

    improvements to security and decentralization

--- ETHEquals1000USD.sol
+++ ETHEquals1000USD.sol
@@ -1,6 +1,52 @@
 pragma solidity ^0.4.11;
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 contract owned {
     address public owner;
 
@@ -20,53 +66,76 @@ contract owned {
 
 contract ETHEquals1000USD is usingOraclize, owned {
 
-    uint public totalGuesses = 0;
+    using SafeMath for uint;
+    using SafeMath for uint256;
+
     bool public priceConfirmedOver1000 = false;
+    bool public checkPriceCalled = false;
+    uint public totalGuesses = 0;
     uint public totalAmount = 0;
+    uint public winningTimestamp;
+    uint internal CONTRACT_CREATED = block.timestamp;
+    uint internal SECS_IN_ONE_WEEK = 604800;
+    uint internal SECS_IN_30_DAYS = 2592000;
     mapping (address => uint) public participantId;
     Participant[] public participants;
+    Nominee[] public nominees;
 
     event EtherReceived(uint amount, uint total);
     event SomebodyGuessed(address guesser, uint timestamp, uint guesses);
-    event OraclizeResult(string message, uint result, uint timestamp);
-    event WinnerAnnounced(string message, address winner, uint amount);
+    event NewNomination(address nominee, uint timestamp, uint diff);
+    event OraclizeResult(string message, string result, uint timestamp);
+    event WinnerAnnounced(string message, address winner, uint diff, uint amount);
 
     struct Participant {
         address participant;
         uint participantSince; // when user made guess
         uint guess; // UNIX timestamp of user's guess
+        bool hasGuessed; // ensure user can't change guess as price approaches closer to 1000
         uint diff; // diff between winning time and guessed time, assigned once ETH hits 1000 USD
     }
 
+    struct Nominee {
+        address nominee;
+        uint diff;
+    }
+
     function ETHEquals1000USD() {
-        //instantiate first guess with null values to avoid errors
-        makeGuess(0, now);
+        makeGuess(0, now); //instantiate first guess with null values to avoid errors, see Ethereum DAO example contract
     }
 
     // participants make guess by providing UNIX timestamp when they believe ETH >= 1000 USD, as determined by Kraken API
     function makeGuess(address targetParticipant, uint timestamp) public {
+        require(CONTRACT_CREATED + SECS_IN_30_DAYS > now); // establishes guessing period of 30 days following contract creation
         uint id = participantId[targetParticipant];
+
+        // runs on contract creation only
         if (id == 0) {
             participantId[targetParticipant] = participants.length;
             id = participants.length++;
         }
-        participants[id] = Participant({participant: targetParticipant, participantSince: now, guess: timestamp, diff: 0});
-        totalGuesses++;
-        SomebodyGuessed(msg.sender, timestamp, totalGuesses);
+
+        // prevent user from changing guess, something that may happen a lot as price approaches 1000
+        if (participants[id].hasGuessed != true){
+            participants[id] = Participant({participant: targetParticipant, participantSince: now, guess: timestamp, hasGuessed: true, diff: now});
+            totalGuesses++;
+            SomebodyGuessed(targetParticipant, timestamp, totalGuesses);
+        }
     }
 
     function() payable public {
-        totalAmount += msg.value;
+        totalAmount = totalAmount.add(msg.value);
         EtherReceived(msg.value, totalAmount);
     }
 
     // Oracalize callback - https://docs.oraclize.it
-    function __callback(bytes32 myid, string _result) {
+    function __callback(bytes32 myid, string result) {
         if (msg.sender != oraclize_cbAddress()) throw;
-        uint result = stringToUint(_result); //convert Oracalize result to uint
+        uint _result = stringToUint(result); //convert Oracalize result to uint
         OraclizeResult("Price checked", result, now);
-        if (result >= 100000000){ // this number is 1000USD, but `result` loses decimal after string/uint conversion and Kraken API has 5 trailing zeros in return value of price
+        if (_result >= 100000000){ // this number is 1000USD, but `result` loses decimal after string/uint conversion and Kraken API has 5 trailing zeros in return value of price
             priceConfirmedOver1000 = true;
+            winningTimestamp = block.timestamp;
         }
     }
 
@@ -78,52 +147,56 @@ contract ETHEquals1000USD is usingOraclize, owned {
                 result = result * 10 + (uint(b[i]) - 48); // bytes and int are not compatible with the operator -.
             }
         }
-        return result; // this was missing
+        return result;
     }
 
     // Query Kraken API to check ETHUSD price, will trigger __callback method from Oracalize
-    function checkPrice() onlyOwner {
+    function checkPrice() public {
+        require(!priceConfirmedOver1000); //ensure method can't be called again once oracle confirms we've crossed 1000, thereby changing winningTimestamp.
         oraclize_query("URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0");
     }
 
-    function payout(uint winningTimestamp) onlyOwner payable {
-        require(priceConfirmedOver1000);
+    function claimCheckPriceReward() {
+        require(!checkPriceCalled && priceConfirmedOver1000); // reward first person to call this function with 10% of totalAmount AFTER checkPrice() is successfully called
+        uint tenPercentBalance = totalAmount.div(10);
+        msg.sender.transfer(tenPercentBalance);
+        totalAmount = totalAmount.sub(tenPercentBalance);
+        checkPriceCalled = true;
+    }
 
-        // set diff between participant's guess and winningTimestamp
-        for(uint i = 0; i < participants.length; i++){
-            Participant storage p = participants[i];
-            // get difference between guess and winning time, whether guess falls before or after winning time
-            if (winningTimestamp > p.guess){
-                p.diff = winningTimestamp - p.guess;
-            } else {
-                p.diff = p.guess - winningTimestamp;
-            }
+    function nominateSelfAsWinner(address possibleWinner) public {
+        require(priceConfirmedOver1000); // can only be called once oracle has established we are over 1000
+        uint id = participantId[possibleWinner];
+        Participant storage p = participants[id];
+        require(p.hasGuessed); // ensure nominee is actually a participant already
+
+        if (winningTimestamp > p.guess){
+            p.diff = winningTimestamp.sub(p.guess);
+        } else {
+            p.diff = p.guess.sub(winningTimestamp);
         }
+        nominees.push(Nominee({nominee: possibleWinner, diff: p.diff}));
+        NewNomination(possibleWinner, p.guess, p.diff);
+    }
 
-        uint lowestDiff = 1529757632; // initiate variable with a recent timestamp
-
-        //iterate over the diffs to establish what the lowest diff is
-        for(uint j = 0; j < participants.length; j++){
-            Participant storage p2 = participants[j];
-            if (p2.diff < lowestDiff){
-                lowestDiff = p2.diff;
-            }
-        }
-
+    function payout() payable public {
+        require(winningTimestamp + SECS_IN_ONE_WEEK > now); // establishes nomination period of 1 week following price hitting 1000
+        uint lowestDiff = 1529757632; // initiate variable with arbitrarily high timestamp, corresponds to 20180623 date
         address winner;
 
-        //determine which participant has the lowest diff and is thus the winner
-        for(uint k = 0; k < participants.length; k++){
-            Participant storage p3 = participants[k];
-            if (p3.diff == lowestDiff){
-                winner = p3.participant;
+        //determine which nominee has the lowest diff and is thus the winner
+        for(uint i = 0; i < nominees.length; i++){
+            if (nominees[i].diff < lowestDiff){
+                lowestDiff = nominees[i].diff;
+                winner = nominees[i].nominee;
             }
         }
 
         winner.transfer(totalAmount);
-        WinnerAnnounced("Congrats!", winner, totalAmount);
+        WinnerAnnounced("Congrats!", winner, lowestDiff, totalAmount);
     }
 
+    // allows contract owner to reclaim ETH in the event of some logic error
     function endContract() onlyOwner public {
         selfdestruct(owner);
     }
--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 ## About the contract
 
-See original [Reddit post](https://www.reddit.com/r/ethtrader/comments/8sn7ir/predict_the_exact_day_eth_will_reach_1000_again/) for the origin story.
+See original [Reddit post](https://www.reddit.com/r/ethtrader/comments/8sn7ir/predict_the_exact_day_eth_will_reach_1000_again/) for the origin story. Development threads from ETHTrader community [here](https://www.reddit.com/r/ethtrader/comments/8ta4l4/eth_at_1000_usd_smart_contract/) and [here]().
 
 This contract allows participants to guess when they believe the price of ether (ETH) will next reach 1000 USD. Under the assumption that [u/Jimmyn3wbert](https://www.reddit.com/user/Jimmyn3wbert) will follow through with his promise and use this particular contract as the means of fulfilling it, the person whose guess is closest to when ETH hits the mark wins.
 
+## Test contract
+
+Deployed to `Rinkeby`, [this contract](https://rinkeby.etherscan.io/address/0x8d928445f180fe2e00bdd18eb9bf73d17e11f8d6) is identical to the solidity contract here except the threshold for ETHUSD price is set to 475USD.
+
 ## Functionality
 
-1. Participants include both a [UNIX timestamp](https://www.unixtimestamp.com/index.php) and the address that they want to receive the ETH in when making a guess. Only one guess per address is possible.
+1. Participants include both a [UNIX timestamp](https://www.unixtimestamp.com/index.php) and an address that will receive the ETH when making a guess.
 
-2. Once the `checkPrice` function is called, which can only be called by the contract owner, and assuming the price is greater than 1000 USD based on Kraken's last trade between the ETHUSD pairing on their API, `priceConfirmedOver1000` is set to true and the `payout` function can be called.
+* Guessing period is open for one month following contract creation.
+* One guess per address, and participant can't change their guess once made. This is done to prevent people from changing their guess either once their time has passed or as we approach closer to 1000USD.
 
-3. `payout` iterates through all the various participants and finds the one whose timestamp guess is closest to the winning timestamp, whether their guess falls before or after the winning timestamp. Note, there is nothing stopping the owner from putting a false timestamp in when he calls `payout`. Assuming good faith, he should call `checkPrice`, wait for Oraclize to return the `result` from the API call, then provide the timestamp returned in the event log (from when Oraclize was queried) in the `payout` function.
+2. When `checkPrice` is called, and assuming the price is greater than 1000 USD based on Kraken's last trade between the ETHUSD pairing on their API, the oracle callback function sets `priceConfirmedOver1000` to true and `winningTimestamp` is set to the current block timestamp.
+
+* To be clear, `winningTimestamp` is determined by the blockchain and not the instant price breaks 1000 on Kraken. Thus, all functions (except `selfdestruct`) can be called by anyone.
+* As an incentive mechanism to call `checkPrice` as close as possible to when we actually pass 1000USD, the first person to call `claimCheckPriceReward` **AFTER** `priceConfirmedOver1000` is set to true by the oracle is rewarded 10% of the contract's `totalAmount`. To be clear, the person who gets the reward is not the person who called `checkPrice` but the person who called `claimCheckPriceReward` after `checkPrice` is called successfully. The reason for this is because there are possible gas errors by having the oracle callback call other functions that are `payable`.
+
+3. Now the nomination period begins. Instead of iterating over the myriad guesses that may exist, creating possible gas issues, people who think they have a chance of winning nominate themselves, creating a smaller subset of possible winners.
+
+* The nomination period lasts for seven days following when `winningTimestamp` is established by the oracle. `payout` can be called by anyone following this seven day period, and the address who got closest to the winning timestamp takes all that remains in `totalAmount`
+* Anyone can add more ETH to the total pot, though bear in mind that the bigger it gets the more incentivized the contract owner is to call `selfdestruct` and take it all for himself
 
 ---
 
@@ -20,7 +33,9 @@ This is an open source project. Contributions are welcomed & encouraged! :smile:
 
 ## TODO
 * Audit and code review
+* Testing on Rinkeby
 
 ## References
 * [Oracalize](https://docs.oraclize.it/)
 * [Kraken API](https://www.kraken.com/help/api#get-ticker-info)
+* [UNIX Timestamp converter](https://www.unixtimestamp.com/index.php)
